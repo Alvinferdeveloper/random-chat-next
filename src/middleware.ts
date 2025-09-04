f911b9c7caccff8d08664210b98680a385109d28
Albin <alvinfer67@gmail.com>
2025-09-04 14:27:45 -0600

    feat(middleware): implement middleware to handle authenticaded users

---
+++ src/middleware.ts
@@ -0,0 +1,47 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+const publicRoutes = ['/', '/login'];
+
+const SESSION_URL = `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001'}/api/v1/user/session`;
+
+export async function middleware(request: NextRequest) {
+    const { pathname } = request.nextUrl;
+    const isPublicRoute = publicRoutes.includes(pathname);
+
+    let isAuthenticated = false;
+    try {
+        const response = await fetch(SESSION_URL, {
+            headers: request.headers,
+        });
+
+        if (response.ok) {
+            isAuthenticated = true;
+        }
+    } catch (error) {
+        console.error('Error al verificar la sesión:', error);
+        isAuthenticated = false;
+    }
+
+    if (isPublicRoute && isAuthenticated) {
+        return NextResponse.redirect(new URL('/rooms', request.url));
+    }
+
+    if (!isPublicRoute && !isAuthenticated) {
+        return NextResponse.redirect(new URL('/login', request.url));
+    }
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        /*
+         * Coincidir con todas las rutas de petición excepto las siguientes:
+         * - api (rutas de API del frontend)
+         * - _next/static (archivos estáticos)
+         * - _next/image (archivos de optimización de imágenes)
+         * - favicon.ico (archivo de favicon)
+         */
+        '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    ],
+};
